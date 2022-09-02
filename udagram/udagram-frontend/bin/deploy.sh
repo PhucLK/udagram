3807b92185cfb4794f5d20757b601e6ef716c51c
phuclk <phuclkcarryonto04@gmail.com>
2022-09-02 11:45:36 +0700

    update deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,1 @@
-aws s3 cp --recursive --acl public-read ./www s3://mybk435567672677/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://mybk435567672677/
+aws s3 cp --recursive --acl public-read ./www s3://udagram-hosting/
