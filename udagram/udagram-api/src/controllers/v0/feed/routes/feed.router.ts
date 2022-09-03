a796450b50e43a4a54291e1a736841e0d888e781
phuclk <phuclkcarryonto04@gmail.com>
2022-09-03 09:12:15 +0700

    update

--- udagram/udagram-api/src/controllers/v0/feed/routes/feed.router.ts
+++ udagram/udagram-api/src/controllers/v0/feed/routes/feed.router.ts
@@ -1,6 +1,5 @@
-import {Router, Request, Response} from 'express';
-import {FeedItem} from '../models/FeedItem';
-import {NextFunction} from 'connect';
+import { Router, Request, Response, NextFunction } from 'express';
+import { FeedItem } from '../models/FeedItem';
 import * as jwt from 'jsonwebtoken';
 import * as AWS from '../../../../aws';
 import * as c from '../../../../config/config';
@@ -9,18 +8,18 @@ const router: Router = Router();
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
   if (!req.headers || !req.headers.authorization) {
-    return res.status(401).send({message: 'No authorization headers.'});
+    return res.status(401).send({ message: 'No authorization headers.' });
   }
 
   const tokenBearer = req.headers.authorization.split(' ');
   if (tokenBearer.length != 2) {
-    return res.status(401).send({message: 'Malformed token.'});
+    return res.status(401).send({ message: 'Malformed token.' });
   }
 
   const token = tokenBearer[1];
   return jwt.verify(token, c.config.jwt.secret, (err, decoded) => {
     if (err) {
-      return res.status(500).send({auth: false, message: 'Failed to authenticate.'});
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate.' });
     }
     return next();
   });
@@ -28,7 +27,7 @@ export function requireAuth(req: Request, res: Response, next: NextFunction) {
 
 // Get all feed items
 router.get('/', async (req: Request, res: Response) => {
-  const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
+  const items = await FeedItem.findAndCountAll({ order: [['id', 'DESC']] });
   items.rows.map((item) => {
     if (item.url) {
       item.url = AWS.getGetSignedUrl(item.url);
@@ -39,45 +38,45 @@ router.get('/', async (req: Request, res: Response) => {
 
 // Get a feed resource
 router.get('/:id',
-    async (req: Request, res: Response) => {
-      const {id} = req.params;
-      const item = await FeedItem.findByPk(id);
-      res.send(item);
-    });
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const item = await FeedItem.findByPk(id);
+    res.send(item);
+  });
 
 // Get a signed url to put a new item in the bucket
 router.get('/signed-url/:fileName',
-    requireAuth,
-    async (req: Request, res: Response) => {
-      const {fileName} = req.params;
-      const url = AWS.getPutSignedUrl(fileName);
-      res.status(201).send({url: url});
-    });
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { fileName } = req.params;
+    const url = AWS.getPutSignedUrl(fileName);
+    res.status(201).send({ url: url });
+  });
 
 // Create feed with metadata
 router.post('/',
-    requireAuth,
-    async (req: Request, res: Response) => {
-      const caption = req.body.caption;
-      const fileName = req.body.url; // same as S3 key name
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const caption = req.body.caption;
+    const fileName = req.body.url; // same as S3 key name
 
-      if (!caption) {
-        return res.status(400).send({message: 'Caption is required or malformed.'});
-      }
+    if (!caption) {
+      return res.status(400).send({ message: 'Caption is required or malformed.' });
+    }
 
-      if (!fileName) {
-        return res.status(400).send({message: 'File url is required.'});
-      }
+    if (!fileName) {
+      return res.status(400).send({ message: 'File url is required.' });
+    }
 
-      const item = await new FeedItem({
-        caption: caption,
-        url: fileName,
-      });
-
-      const savedItem = await item.save();
-
-      savedItem.url = AWS.getGetSignedUrl(savedItem.url);
-      res.status(201).send(savedItem);
+    const item = await new FeedItem({
+      caption: caption,
+      url: fileName,
     });
+
+    const savedItem = await item.save();
+
+    savedItem.url = AWS.getGetSignedUrl(savedItem.url);
+    res.status(201).send(savedItem);
+  });
 
 export const FeedRouter: Router = router;
